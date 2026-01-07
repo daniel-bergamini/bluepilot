6f9f1f934d96d67dc9c0f7542ee449616962d1c0
Daniel Bergamini <daniel.bergamini@gmail.com>
2026-01-07 11:54:48 -0500

    register bp portal params in params keys

--- common/params_keys.h
+++ common/params_keys.h
@@ -218,6 +218,12 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"SunnylinkEnabled", {PERSISTENT, BOOL, "1"}},
     {"SunnylinkTempFault", {CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION, BOOL, "0"}},
 
+    // Bluepilot portal params
+    {"BPPortalEnabled", {PERSISTENT | BACKUP, BOOL, "0"}},
+    {"BPPortalPort", {PERSISTENT | BACKUP, INT, "8088"}},
+    {"BPPortalCrashCount", {PERSISTENT, INT, "0"}},
+    {"BPPortalLastCrash", {PERSISTENT, INT, "0"}},
+
     // Backup Manager params
     {"BackupManager_CreateBackup", {PERSISTENT, BOOL}},
     {"BackupManager_RestoreVersion", {PERSISTENT, STRING}},
